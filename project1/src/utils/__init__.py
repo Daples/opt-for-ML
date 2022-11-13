d6f3208944f7867a119c7aec3c3b7aaf6a41b1e7
Daples <daples1@gmail.com>
2022-11-13 22:38:05 +0100

    [ utils ] Added Gaussian kernel.

--- project1/src/utils/__init__.py
+++ project1/src/utils/__init__.py
@@ -18,3 +18,22 @@ def euclidean(point: np.ndarray, matrix: np.ndarray) -> np.ndarray:
     """
 
     return np.sum(np.power(np.subtract(matrix, point), 2), axis=1)
+
+
+def gaussian_kernel(
+    point: np.ndarray, matrix: np.ndarray, gamma: float = 0.5
+) -> np.ndarray:
+    """It calculates the Gaussian kernelized distanced from a point to each row of a
+    matrix.
+
+    Parameters
+    ----------
+    point: numpy.ndarray
+        The reference point of dimension`m`.
+    matrix: numpy.ndarray
+        The matrix of points to measure distance. It is of dimensions `n x m`.
+    gamma: float, optional
+        The scaling factor of the Gaussian kernel.
+    """
+
+    return np.exp(-gamma * euclidean(point, matrix))
