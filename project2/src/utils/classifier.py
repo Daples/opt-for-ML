b1be0028e825de55e9d2a1a2281097f03cd53f98
Daples <daples1@gmail.com>
2022-12-15 10:44:21 +0100

    [ uitls ] Added classified interface.

---
+++ project2/src/utils/classifier.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
+
+
+class ClassificationModel:
+    """"""
+
+    def __init__(self, model: SVC, k_cross_val: int = 5) -> None:
+        self.model: SVC = model
+        self.k_cross_val: int = k_cross_val
+
+    def eval(
+        self,
+        X: np.ndarray,
+        y: np.ndarray,
+        hyperparameters: np.ndarray,
+        names: list[str],
+    ) -> float:
+        """It evaluates the performance of the model on the"""
+
+        hyperparameters = np.power(10, hyperparameters)
+        values = hyperparameters.tolist()
+        self.model = self.model.set_params(**dict(list(zip(names, values))))
+        scores = cross_val_score(
+            self.model, X, y, cv=self.k_cross_val, scoring="accuracy"
+        )
+        return scores.max()
