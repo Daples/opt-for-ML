8eed3847e520d8797f3f09b113baa063543c700b
Daples <daples1@gmail.com>
2022-12-17 15:18:47 +0100

    [ utils ] Added missing documentation.

--- project2/src/utils/classifier.py
+++ project2/src/utils/classifier.py
@@ -4,7 +4,15 @@ from sklearn.svm import SVC
 
 
 class ClassificationModel:
-    """"""
+    """A wrapper for sklearn's SVC model (mostly for safe typing reasons).
+
+    Attributes
+    ----------
+    model: sklearn.svm.SVC
+        The Support Vector Classifier object.
+    k_cross_val: int
+        The number of folds for cross validation.
+    """
 
     def __init__(self, model: SVC, k_cross_val: int = 5) -> None:
         self.model: SVC = model
@@ -17,7 +25,24 @@ class ClassificationModel:
         hyperparameters: np.ndarray,
         names: list[str],
     ) -> float:
-        """It evaluates the performance of the model on the"""
+        """It evaluates the performance of the model on the given hyperparameters.
+
+        Parameters
+        ----------
+        X: numpy.ndarray
+            The dataset.
+        y: numpy.ndarray
+            The data labels.
+        hyperparameters: numpy.ndarray
+            The hyperparameters to evaluate the model on.
+        names: list[str]
+            The hyperparameters names.
+
+        Returns
+        -------
+        float
+            The model performance.
+        """
 
         hyperparameters = np.power(10, hyperparameters)
         values = hyperparameters.tolist()
