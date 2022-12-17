07ddffc2aa8e608dce406366d7830445da8ca190
Daples <daples1@gmail.com>
2022-12-17 15:19:44 +0100

    [ utils ] Added projection operator, updated default values and improved documentation.

--- project2/src/utils/optimize.py
+++ project2/src/utils/optimize.py
@@ -5,51 +5,85 @@ import numpy as np
 
 def incremental_search(
     f: Callable[[float], float],
-    starting_point: float = 0,
-    step: float = 1e-5,
+    max_step: float,
+    starting_point: float = 1e-18,
+    step: float = 0.05,
 ) -> tuple[float, float]:
-    """It uses accelerated incremental search to find the upper limit where the maximum is
-    contained.
+    """It uses accelerated incremental search to find the upper limit where a local
+    maximum is contained.
 
     Parameters
     ----------
     f: (float) -> float
         The objective function.
+    max_step: float
+        The maximum possible step for the gradient descent.
     starting_point: float, optional
         The starting point of the incremental search.
     step: float, optional
         The step size for incremental search.
+
+    Returns
+    -------
+    float
+        The lower bound of the estimated interval.
+    float
+        The upper bound of the estimated interval.
     """
 
     lower = 0
     upper = starting_point
     prev_val = f(starting_point)
     i = 1
-    shift = lambda i: starting_point + i * step
-    while True:
-        current_point = shift(i)
-        print(current_point)
+    shift = lambda i: (i - 1) * step
+    print("Running incremental search...")
+    while shift(i) < max_step:
+        current_point = starting_point + shift(i)
         current_val = f(current_point)
         if current_val < prev_val:
-            lower = shift(i - 1)
-            upper = shift(i + 1)
             break
         prev_val = current_val
         i += 1
+    lower = starting_point + shift(i - 1)
+    upper = starting_point + shift(i + 1)
+    print("Finished incremental search")
 
     return lower, upper
 
 
-def backtracking(f: Callable, grad_f: Callable, beta, x0) -> float:
-    """Minimization"""
+def backtracking(
+    f: Callable[[np.ndarray], float],
+    grad_f: Callable[[np.ndarray], np.ndarray],
+    beta: float,
+    x: np.ndarray,
+) -> float:
+    """Optimization of the step size via backtracking.
 
-    x = x0
+    Parameters
+    ----------
+    f: (numpy.ndarray) -> float
+        The objective function.
+    grad_f: (numpy.ndarray) -> np.ndarray
+        The gradient of the objective function.
+    beta: float
+        The step scaling parameter.
+    x: numpy.ndarray
+        The point to evaluate the objective function at.
+
+    Returns
+    -------
+    float
+        The estimated step size.
+    """
+
     step = 1
+    i = 1
     while True:
         grad = grad_f(x)
-        if f(x - step * grad) <= f(x) - step / 2 * (grad.dot(grad)):
+        if f(x - step * grad) <= f(x) - step * 1 / 2 * (grad.dot(grad)):
             break
         step = beta * step
+        i += 1
     return step
 
 
@@ -67,6 +101,13 @@ def golden_section_search(
         The interval where a local maximum is guaranteed.
     tol: float
         The relative width of the resulting interval.
+
+    Returns
+    -------
+    float
+        The lower bound of the estimated interval.
+    float
+        The upper bound of the estimated interval.
     """
 
     lower, upper = bounds
@@ -92,7 +133,7 @@ def golden_section_search(
 
 
 def gradient(
-    f: Callable[[np.ndarray], float], point: np.ndarray, delta: float = 1
+    f: Callable[[np.ndarray], float], point: np.ndarray, delta: float = 1e-9
 ) -> np.ndarray:
     """A finite difference gradient approximation.
 
@@ -108,7 +149,7 @@ def gradient(
     Returns
     -------
     numpy.ndarray
-        The gradient.
+        The gradient vector.
     """
 
     n = point.shape[0]
@@ -121,3 +162,30 @@ def gradient(
         shifted_f = f(point + shift)
         grad[i] = (shifted_f - f_val) / delta
     return grad
+
+
+def project(point: np.ndarray, intervals: np.ndarray) -> np.ndarray:
+    """It projects a point inside the input intervals.
+
+    Parameters
+    ----------
+    point: numpy.ndarray
+        The point in question.
+    intervals: numpy.ndarray
+        The domain intervals.
+
+    Returns
+    -------
+    numpy.ndarray
+        The projected point.
+    """
+
+    dim = point.shape[0]
+    for i in range(dim):
+        lower = intervals[i, 0]
+        upper = intervals[i, 1]
+        if point[i] < lower:
+            point[i] = lower
+        elif point[i] > upper:
+            point[i] = upper
+    return point
