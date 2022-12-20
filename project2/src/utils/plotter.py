842ff896d2c3da2bbdfae7c2af53972dc39506df
Daples <daples1@gmail.com>
2022-12-20 16:05:48 +0100

    [ utils ] Added additional plotting functions. Added init/clear methods.

--- project2/src/utils/plotter.py
+++ project2/src/utils/plotter.py
@@ -25,6 +25,13 @@ class Plotter:
         plt.cla()
         plt.clf()
 
+    @staticmethod
+    def __setup_config__() -> None:
+        """It sets up the matplotlib configuration."""
+
+    plt.rc("text", usetex=True)
+    plt.rcParams.update({"font.size": 18})
+
     @classmethod
     def _add_folder(cls, path: str) -> str:
         """It adds the default folder to the input path.
@@ -50,6 +57,10 @@ class Plotter:
         matrix: np.ndarray,
         iteration_path: np.ndarray,
         iteration: int,
+        xlabel: str = "$C$",
+        ylabel: str = "$\gamma$",
+        xlim: list[float] = [0, 9],
+        ylim: list[float] = [-10, 0],
     ) -> None:
         """It stores the contour of an acquisition function maximization.
 
@@ -63,7 +74,18 @@ class Plotter:
             The surface values.
         iteration_path: numpy.ndarray
             The optimization path of points.
+        xlabel: str, optional
+            The label for the x-axis. Default: "$C$".
+        ylabel: str, optional
+            The label for the y-axis. Default: "$\gamma$"
+        xlim: list[float], optional
+            The x-axis limits. Default: [0, 9]
+        ylim: list[float], optional
+            The y-axis limits. Default: [-10, 0]
         """
+
+        cls.clear()
+        cls.__setup_config__()
 
         plt.contourf(x, y, matrix, cls._levels)
         plt.colorbar()
@@ -71,6 +93,10 @@ class Plotter:
         plt.plot(iteration_path[:, 0], iteration_path[:, 1], "r-", linewidth=1)
         plt.scatter(iteration_path[:, 0], iteration_path[:, 1], c="k", s=4)
         plt.scatter(iteration_path[-1, 0], iteration_path[-1, 1], c="w", s=4)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.xlim(xlim)
+        plt.ylim(ylim)
 
         path = f"bo_iter_{iteration}.pdf"
         plt.savefig(cls._add_folder(path), bbox_inches="tight")
@@ -78,7 +104,15 @@ class Plotter:
 
     @classmethod
     def get_heatmatp(
-        cls, x: np.ndarray, y: np.ndarray, matrix: np.ndarray, path: str
+        cls,
+        x: np.ndarray,
+        y: np.ndarray,
+        matrix: np.ndarray,
+        path: str,
+        xlabel: str = "$C$",
+        ylabel: str = "$\gamma$",
+        xlim: list[float] = [0, 9],
+        ylim: list[float] = [-10, 0],
     ) -> None:
         """It saves the heatmap of a matrix in the specified path.
 
@@ -92,17 +126,116 @@ class Plotter:
             The input matrix.
         path: str
             The filename to save the heatmap.
+        xlabel: str, optional
+            The label for the x-axis. Default: "$C$".
+        ylabel: str, optional
+            The label for the y-axis. Default: "$\gamma$"
+        xlim: list[float], optional
+            The x-axis limits. Default: [0, 9]
+        ylim: list[float], optional
+            The y-axis limits. Default: [-10, 0]
         """
+
+        cls.clear()
+        cls.__setup_config__()
 
         extent = [x[0], x[-1], y[0], y[-1]]
         fig, axs = plt.subplots(1, 1)
         heatmap = axs.imshow(
-            matrix,
+            matrix.T,
             cmap="winter",
             interpolation="nearest",
             origin="lower",
             extent=extent,
         )
         fig.colorbar(heatmap)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.xlim(xlim)
+        plt.ylim(ylim)
         plt.savefig(cls._add_folder(path), bbox_inches="tight")
+
+    @classmethod
+    def get_scatter(
+        cls,
+        x: np.ndarray,
+        y: np.ndarray,
+        path: str,
+        xlabel: str = "$C$",
+        ylabel: str = "$\gamma$",
+        xlim: list[float] = [0, 9],
+        ylim: list[float] = [-10, 0],
+    ) -> None:
+        """It plots the scatter plot for the explored points.
+
+        Parameters
+        ----------
+        x: numpy.ndarray
+            The first dimension array.
+        y: numpy.ndarray
+            The second dimension array.
+        path: str
+            The path to save the figure.
+        xlabel: str, optional
+            The label for the x-axis. Default: "$C$".
+        ylabel: str, optional
+            The label for the y-axis. Default: "$\gamma$"
+        xlim: list[float], optional
+            The x-axis limits. Default: [0, 9]
+        ylim: list[float], optional
+            The y-axis limits. Default: [-10, 0]
+        """
+
         cls.clear()
+        cls.__setup_config__()
+
+        plt.scatter(x, y, c="b", s=8)
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.grid()
+        plt.xlim(xlim)
+        plt.ylim(ylim)
+        plt.savefig(cls._add_folder(path), bbox_inches="tight")
+
+    @classmethod
+    def get_misclassification_plot(
+        cls,
+        performances: np.ndarray,
+        performance_grid_search: float,
+        path: str,
+        xlabel: str = "Iteration",
+        ylabel: str = "Missclassification error",
+    ) -> None:
+        """It plots the best-so-far misclassification error between iterations.
+
+        Parameters
+        ----------
+        performances: numpy.ndarray
+            The performances per iteration.
+        performance_grid_search: float
+            Th best performance found from grid search.
+        path: str
+            The path to save the figure.
+        xlabel: str, optional
+            The label for the x-axis. Default: "Iteration".
+        ylabel: str, optional
+            The label for the y-axis. Default: "$Misclassfication error"
+        """
+
+        cls.clear()
+        cls.__setup_config__()
+
+        performances = np.maximum.accumulate(performances)
+        plt.plot(1 - performances, "ko", label="BO performance", markersize=2)
+        plt.axhline(
+            y=1 - performance_grid_search,
+            color="r",
+            linestyle="--",
+            label="Grid search best",
+        )
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.ylim([0, 1])
+        plt.legend()
+        plt.grid()
+        plt.savefig(cls._add_folder(path))
