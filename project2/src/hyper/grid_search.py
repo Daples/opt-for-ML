d8f28084f3d46ea806a41bab4a357c40ba4d9e8b
Daples <daples1@gmail.com>
2023-01-07 17:55:24 +0100

    [ hyper ] Added execution time storage.

--- project2/src/hyper/grid_search.py
+++ project2/src/hyper/grid_search.py
@@ -4,6 +4,7 @@ from typing import Any
 import numpy as np
 from numpy.random import Generator, default_rng
 from sklearn.svm import SVC
+from time import time
 from tqdm import tqdm
 
 from utils.classifier import ClassificationModel
@@ -35,6 +36,12 @@ class GridSearchOptimizer:
         The best-so-far performance.
     performace_matrix: numpy.ndarray
         The matrix that stores the performances on the grid.
+    times_matrix: numpy.ndarray
+        The matrix of function evaluation for each point on the grid.
+    iter_times: list[float]
+        The list of execution time per iteration.
+    total_time: float
+        The total execution time of the hyperparameter search.
     """
 
     def __init__(
@@ -42,7 +49,6 @@ class GridSearchOptimizer:
         intervals: np.ndarray,
         N: int,
         names: list[str],
-        generator: Generator | int,
         k_cross_val: int = 5,
     ) -> None:
         self.model: ClassificationModel = ClassificationModel(
@@ -50,18 +56,20 @@ class GridSearchOptimizer:
         )
         self.intervals: np.ndarray = intervals
         self.n_points: int = N
-        self.generator: Generator = default_rng(generator)
         self.hyperparameter_names: list[str] = names
         self.previous_hyperparameters: np.ndarray = np.zeros(
             (0, self.intervals.shape[0])
         )
         self.previous_performances: np.ndarray = np.zeros((0, 1))
-        self.best_hyperparams: np.ndarray = np.zeros(1)
+        self.best_hyperparams: np.ndarray = np.zeros(0)
         self.best_performance: float = 0
-        self.performance_matrix: np.ndarray = np.zeros(1)
+        self.performance_matrix: np.ndarray = np.zeros(0)
         self.domain_matrix: np.ndarray = np.zeros(
             (self.intervals.shape[0], self.n_points)
         )
+        self.times_matrix: np.ndarray = np.zeros(0)
+        self.iter_times: list[float] = []
+        self.total_time: float = -1
 
     def optimize(self, X: np.ndarray, y: np.ndarray) -> None:
         """Use grid search to evaluate the model performance.
@@ -75,6 +83,7 @@ class GridSearchOptimizer:
         """
 
         # Get meshgrid
+        self.total_time = time()
         n_hyperparams = len(self.hyperparameter_names)
         matrix = np.zeros((n_hyperparams, self.n_points))
         indices_matrix = np.zeros((n_hyperparams, self.n_points), dtype=int)
@@ -95,8 +104,10 @@ class GridSearchOptimizer:
         best_performance = 0
         best_hyperparameters = np.zeros(n_hyperparams)
         self.performance_matrix = np.zeros([self.n_points] * n_hyperparams)
+        self.times_matrix = np.zeros([self.n_points] * n_hyperparams)
 
         for i, hyperparameters in enumerate(tqdm(coordinates)):
+            init_time = time()
             h = np.array(hyperparameters)
             performance = self.model.eval(X, y, h, self.hyperparameter_names)
             if performance > best_performance:
@@ -105,9 +116,16 @@ class GridSearchOptimizer:
             self.previous_performances[i] = performance
             self.performance_matrix[indices[i]] = performance
 
+            # Store time
+            t = time() - init_time
+            self.iter_times.append(t)
+            self.times_matrix[indices[i]] = t
+
+        # Store performance
         self.previous_hyperparameters = np.array(coordinates)
         self.best_hyperparams = best_hyperparameters
         self.best_performance = best_performance
+        self.total_time = time() - self.total_time
 
     def get_json(self) -> dict[str, Any]:
         """It returns a dictionary for JSON output formatting.
@@ -121,4 +139,6 @@ class GridSearchOptimizer:
         d = {}
         d["best_performance"] = self.best_performance
         d["best_hyperparameters"] = self.best_hyperparams.tolist()
+        d["iter_times"] = self.iter_times
+        d["total_time"] = self.total_time
         return d
